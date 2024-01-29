6c4e982ea4b1750f9e18af8aa7022396fea09f8d
adebanjo23 <solomon.adebanjo@stu.cu.edu.ng>
2024-01-29 01:09:14 +0100

    Made Updates to urls

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.urls import path
 from .views import signup, activate, resend_activation_link, user_login, upload_pdf, query_pdf, delete_pdf
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
@@ -9,4 +12,7 @@ urlpatterns = [
     path('upload/', upload_pdf, name='upload_pdf'),
     path('query/', query_pdf, name='query_pdf'),
     path('delete/', delete_pdf, name='delete_pdf'),
+    re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 ]
